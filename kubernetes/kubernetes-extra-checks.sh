9948134825151858a7c3cdcc185f27591f217e8b
bilboer <92595570+bilboer@users.noreply.github.com>
2021-12-07 23:43:25 +0100

    Update kubernetes-extra-checks.sh
    * Update the sonobuoy version
    * Use a fixed conformance image (defaults to 1.21.0)

--- kubernetes/kubernetes-extra-checks.sh
+++ kubernetes/kubernetes-extra-checks.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -ex
 
-SONOBUOY_VERSION=${SONOBUOY_VERSION:-0.18.0}
+K8S_VERSION=${K8S_VERSION:-1.21.0}
+SONOBUOY_VERSION=${SONOBUOY_VERSION:-0.55.0}
 SONOBUOY_PARALLEL=${SONOBUOY_PARALLEL:-30}
+SONOBUOY_MODE=${SONOBUOY_MODE:-non-disruptive-conformance}
+SONOBUOY_CONFORMANCE_IMAGE=${SONOBUOY_CONFORMANCE_IMAGE:-k8s.gcr.io/conformance:v${K8S_VERSION}}
 
 function fetch_sonobuoy() {
     rm -rf sonobuoy.*
@@ -9,19 +12,25 @@ function fetch_sonobuoy() {
     tar xvf sonobuoy_"$SONOBUOY_VERSION"_linux_amd64.tar.gz
 }
 
+function  cleanup() {
+   test -x "./sonobuoy" && ./sonobuoy delete --all --wait
+   exit 0
+}
+
 function  run_sonobuoy() {
   if [[ ! -x "./sonobuoy" ]]; then
     fetch_sonobuoy
   fi
 
-  ./sonobuoy delete --all || true
-  ./sonobuoy run --skip-preflight --plugin e2e --e2e-parallel ${SONOBUOY_PARALLEL} --sonobuoy-image projects.registry.vmware.com/sonobuoy/sonobuoy:v${SONOBUOY_VERSION} --mode=non-disruptive-conformance --wait 2>&1
+  ./sonobuoy run --kube-conformance-image=${SONOBUOY_CONFORMANCE_IMAGE} --mode=${SONOBUOY_MODE} --skip-preflight --plugin e2e --e2e-parallel ${SONOBUOY_PARALLEL} --sonobuoy-image projects.registry.vmware.com/sonobuoy/sonobuoy:v${SONOBUOY_VERSION} --wait 2>&1
   ./sonobuoy results $(./sonobuoy retrieve) -m dump | ./parse_results.py failed
-  ./sonobuoy delete --all --wait 2>&1
 }
 
 function main() {
   run_sonobuoy
+  exit 0
 }
+
+trap cleanup EXIT
 
 main
